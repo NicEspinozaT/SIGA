b00bfff4e0e64219b738b0e2ae152dc12d168c8f
Jean Castro <je.castrob@duocuc.cl>
2023-11-15 00:37:39 -0300

    Creación del modelo de admisión

--- SIGA/m_admision/models.py
+++ SIGA/m_admision/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import (
+  Model,
+  CharField,
+  IntegerField,
+  DateTimeField,
+  BigAutoField,
+)
 
-# Create your models here.
+op_estados = [
+  [0, "No Pagado"],
+  [1, "Pendiente"];
+  [2, "Pagado"],
+]
+
+class Matricula(Model):
+  id = BigAutoField (primary_key=True)
+  estado = IntegerField(choices=op_estados)
+  fecha = DateTimeField(auto_now_add=True)
+  periodo = CharField (max_length=20)
+
+  class Meta:
+    db_table = "Matricula"
+
+  def __str__(self):
+    return f"{self.id}"
