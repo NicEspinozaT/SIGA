cf8577ae91700d97523c6360c15a337d481b59e7
Your NameNicEspinozaT <you@example.comnicolasespinoza1985@gmail.com>
2023-11-21 22:55:06 -0300

    Filtrar apoderado y estudiante

--- SIGA/m_admision/urls.py
+++ SIGA/m_admision/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import admision, registrar_apoderado, registrar_estudiante, registrar_matricula
+from .views import admision, registrar_apoderado, registrar_estudiante, registrar_matricula,buscar_apoderado,buscar_estudiante
 
 urlpatterns = [
     path("admision/", admision, name="admision"),
-    path("admision/matricula/", registrar_apoderado, name="matricula"),
+    path("admision/matricula/", buscar_apoderado, name="matricula"),
     path("admision/matricula/registrar_estudiante", registrar_estudiante, name="registroEstudiante"),
+    path("admision/matricula/buscar_apoderado", buscar_apoderado, name="buscarApoderado"),
     path("admision/matricula/registrar_apoderado", registrar_apoderado, name="registrarApoderado"),
+    path("admision/matricula/buscar_estudiante", buscar_estudiante, name="buscarEstudiante"),
     path("admision/matricula/registrar_matricula", registrar_matricula, name="registroMatricula"),
 ]
